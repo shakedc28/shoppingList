d57b52cc3ef0cd81ec24bd151d3150623bb1a28b
shaked cohen <102024128+shakedc28@users.noreply.github.com>
2025-11-14 18:51:35 +0100

    Initialize App component with ShoppingListDetail

---
+++ Update src/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import ShoppingListDetail from './components/ShoppingListDetail/ShoppingListDetail';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <ShoppingListDetail listId="1" />
+    </div>
+  );
+}
+
+export default App;
