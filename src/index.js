c4b75b45ff3d6c27d39eb3f48d51a272f0eddb8a
shaked cohen <102024128+shakedc28@users.noreply.github.com>
2025-11-16 02:21:04 +0100

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,8 +1,1 @@
-import React from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App'
-
-
-const container = document.getElementById('root')
-const root = createRoot(container)
-root.render(<React.StrictMode><App /></React.StrictMode>)
+import App from "./App";
